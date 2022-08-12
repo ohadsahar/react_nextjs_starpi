7cfe503f8b82d0fe4b7e4d4bc4cc75a0023b4067
Ohad Green <ohadgreen@Ohads-MacBook-Pro.local>
2022-08-12 18:04:40 +0300

    removed unorganized imports

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,7 @@
 import type { AppProps } from "next/app";
-import GlobalStyle from "../theme/globalStyle";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../theme/globalStyle";
 import theme from "../theme/theme";
-import { GetStaticProps } from "next";
-import { STARPI_API_URL } from "../config/Config";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
